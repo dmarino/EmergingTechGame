aac9a5f0df7ccbb330c748964ae4461a812ed6dd
Daniela <d.marino10@uniandes.edu.co>
2020-05-28 14:51:55 -0700

    Voice Controll

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{    
+    [SerializeField]
+    private float _moveOffset = 5f;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+          rb = gameObject.GetComponent<Rigidbody>();
+    }
+    private void Update(){
+
+        float xVel = _moveOffset * Input.GetAxis("Horizontal");
+        float zVel = _moveOffset * Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(xVel,0, zVel);
+    }
+
+
+    public void Action()
+    {
+        Debug.Log("i listen!");
+    }
+
+
+}
